b2701f614ea26a53345bf8378b06390199375643
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-01-21 12:04:42 +0600

    Update global styles and enhance WhyMatchMaker component layout

--- src/components/WhyMatchMaker/WhyMatchMaker.tsx
+++ src/components/WhyMatchMaker/WhyMatchMaker.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const WhyMatchMaker = () => {
   return (
-    <div>WhyMatchMaker</div>
-  )
-}
+    <div className="w-full bg-primary px-5 py-16 md:py-32">
+      <div className="w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="space-y-2">
+          <h1 className="text-3xl md:text-5xl font-semibold leading-normal">
+            Why Maker
+          </h1>
+          <h1 className="text-3xl  md:text-5xl font-semibold leading-normal">
+            Match <span className="text-white">Matters</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WhyMatchMaker
+export default WhyMatchMaker;
