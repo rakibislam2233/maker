52ffc516857d0debb870325ffd53f57f261d2abf
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-01-21 09:41:17 +0600

    Refactor Home page layout and add new components for improved structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,25 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1336px",
+      xxl: "1620px",
+    },
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#00A2F3",
+        secondary: "#FF288F",
+        primaryBg: "#3D5E91",
+        hoverText: "#dcbb87",
+      },
+      animation: {
+        "spin-reverse": "spin 1s linear reverse infinite",
       },
     },
   },
