78fc7e0a0eb367c8fb980ad4dd1b8271c49202cd
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-01-21 11:55:04 +0600

    Add HeroSection and HowItWorks components; update package dependencies

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { GoArrowUpRight } from "react-icons/go";
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
+  const spotsLeft = 1472; // Number of spots left
 
-export default HeroSection
+  // Convert the spotsLeft number into an array of digits
+  const spotsArray = spotsLeft.toString().split("");
+  return (
+    <section className="w-full h-full px-5 py-28 md:py-36">
+      <div className="w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+        <div>
+          <h1 className="text-3xl md:text-5xl xl:text-6xl font-bold leading-normal">
+            Find Your <br /> Perfect{" "}
+            <span className="text-secondary">Match</span>
+          </h1>
+          <p className="text-[16px]  mt-3">
+            Our AI-powered matchmaking service connects you with compatible
+            partners based on what truly matters your values, goals, and
+            personality. Ready to meet your match?
+          </p>
+        </div>
+        <div className="w-full flex flex-col text-center justify-center">
+          <h1 className="text-xl font-semibold text-[#696B6F] mb-4">
+            We’re Launching Next Month
+          </h1>
+          <div className="w-full max-w-full border p-10 rounded-xl shadow-md bg-white mx-auto">
+          <div className="flex items-center justify-center gap-2 mb-6">
+            {spotsArray.map((digit, index) => (
+              <div
+                key={index}
+                    className="size-14 flex items-center justify-center border rounded-md bg-[#F4F4F4]"
+              >
+                <span className="text-xl font-bold text-[#696B6F]">{digit}</span>
+              </div>
+            ))}
+            <span className="text-lg text-[#696B6F] ml-2">spots left</span>
+          </div>
+          <hr className="my-9" />
+          <h3 className="text-xl font-semibold text-[#696B6F] mb-4">
+            Claim Your Spot For Free
+          </h3>
+          <div className="flex justify-between items-center gap-1 rounded-full bg-[#F2F3F6] p-1">
+            <input
+              type="email"
+              placeholder="Enter Email"
+              className="w-full py-2 px-5 rounded-l-full outline-none bg-transparent text-[#696B6F]"
+            />
+            <button className="bg-[#00A2F3] text-white px-8 py-3.5 rounded-full font-semibold flex items-center gap-1">
+              Submit
+              <GoArrowUpRight className="size-6"/>
+            </button>
+          </div>
+        </div>
+          </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/components/HowItWorks/HowItWorks.tsx
+++ src/components/HowItWorks/HowItWorks.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import Image from "next/image";
+import step1 from "@/assets/how-work/step1.png";
+import step2 from "@/assets/how-work/step2.png";
+import step3 from "@/assets/how-work/step3.png";
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
-  )
-}
+    <div className="w-full bg-[#EBF7FD] px-5 py-16 md:py-32">
+      <div className="w-full max-w-7xl mx-auto">
+        <h1 className="text-3xl md:text-5xl xl:text-6xl font-bold leading-normal">
+          How It <span className="text-secondary">Works</span>
+        </h1>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 place-items-center place-content-center gap-10 mt-10">
+          {/* Step 1 */}
+          <div className="order-1 md:order-1 space-y-2">
+            <h1 className="text-primary text-2xl font-semibold uppercase">
+              Step 1
+            </h1>
+            <h1>
+              Set your filters and answer 5x short questions about yourself and
+              what you’re looking for
+            </h1>
+          </div>
+          <Image
+            src={step1}
+            alt="step1"
+            width={350}
+            height={300}
+            className="order-2 md:order-2 mx-auto"
+          />
+          {/* Step 2 */}
+          <div className="order-3 md:order-4 space-y-2">
+            <h1 className="text-secondary text-2xl font-semibold uppercase">
+              Step 2
+            </h1>
+            <h1>
+              AI matches your answers to all users in our network to find your
+              perfect matches. We send you 5x matches every Sunday (along with
+              date ideas based on your mutual interests, location, and
+              availability)
+            </h1>
+          </div>
+          <Image
+            src={step2}
+            alt="step2"
+            width={350}
+            height={300}
+            className="order-4 md:order-3 mx-auto"
+          />
+          {/* Step 3 */}
+          <div className="order-5 md:order-5 space-y-2">
+            <h1 className="text-primary text-2xl font-semibold uppercase">
+              Step 3
+            </h1>
+            <h1>
+              Get matched with your perfect matches, chat, and set up a date
+            </h1>
+          </div>
+          <Image
+            src={step3}
+            alt="step3"
+            width={350}
+            height={300}
+            className="order-6 md:order-6 mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HowItWorks
+export default HowItWorks;
